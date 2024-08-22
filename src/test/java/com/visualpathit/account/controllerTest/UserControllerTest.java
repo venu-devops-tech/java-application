f0a540fff32059d9e8c1f0af22e19d4fb0c22860
venu-devops-tech <venugopal.malli.kulkarni@gmail.com>
2024-08-22 09:18:51 +0530

    Update UserControllerTest.java

--- src/test/java/com/visualpathit/account/controllerTest/UserControllerTest.java
+++ src/test/java/com/visualpathit/account/controllerTest/UserControllerTest.java
@@ -8,10 +8,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+// import org.mockito.InjectMocks;
+// import org.mockito.Mock;
 import org.mockito.*;
-import org.mockito.MockitoAnnotations;
+// import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
